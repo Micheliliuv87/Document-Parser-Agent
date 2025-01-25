ec66aa44481e96df21b1297380c9ee7e523835bb
MichV <micheliliuv87@gmail.com>
2025-01-25 18:56:32 -0500

    prepare.py

--- prepare.py
+++ prepare.py
@@ -1,3 +1,3 @@
 from Packages.Year_Sheet_fun import process_excel_to_yearly_files
 
-process_excel_to_yearly_files("GOOG00012708_Rog_1_and_2_Response.XLSX") #the file can be put under the same direcotry of this file 
+process_excel_to_yearly_files("Your_Document.XLSX") #the file can be put under the same direcotry of this file 
